e04c4a0d3f7797cbb1a91e511febd92ab347f291
henderjon <jhenderson@capstonedigital.com>
2015-03-24 10:27:58 -0500

    less traits ...

--- src/Pinger.php
+++ src/Pinger.php
@@ -5,24 +5,19 @@ namespace Pinger;
 class Pinger {
 
 	/**
-	 * We need a little extra help
-	 */
-	use HTTPContextHelperTrait;
-
-	/**
 	 * Explicitly define the base url/version
 	 */
-	protected $apiURL = "";
-
-	/**
-	 * Explicitly define the endpoints
-	 */
-	protected $endpoints = array();
+	protected $apiURL;
 
 	/**
 	 * hold the cURL connection info
 	 */
-	protected $responseInfo = array();
+	protected $responseMeta = array();
+
+	/**
+	 * hold the cURL connection info
+	 */
+	protected $response;
 
 	/**
 	 *
@@ -35,22 +30,44 @@ class Pinger {
 	const METHOD_POST = "POST";
 
 	/**
+	 * attribute(s) required by stream_context_create()
+	 */
+	protected $user_agent = "henderjon/pinger PHP stream_context_create";
+
+	/**
+	 * attribute to store the headers to be used for the request
+	 */
+	protected $headers = array();
+
+	/**
+	 * header delimiter
+	 */
+	private $DELIM = "\r\n";
+
+	/**
 	 * Description
 	 * @param type $user
 	 * @param type $pass
 	 * @return type
 	 */
-	function __construct($api, array $endpoints){
+	function __construct($api){
 		$this->apiURL    = $api;
-		$this->endpoints = $endpoints;
 	}
 
 	/**
 	 * method to get the info of the last request
 	 * @return array
 	 */
-	function getInfo(){
-		return $this->responseInfo;
+	function getResponseMeta(){
+		return $this->responseMeta;
+	}
+
+	/**
+	 * method to get the info of the last request
+	 * @return array
+	 */
+	function getResponse(){
+		return $this->responseMeta;
 	}
 
 	/**
@@ -94,14 +111,10 @@ class Pinger {
 	protected function normalizeEndpoint($endpoint, array $data = array()){
 		$url = "";
 
-		// if(in_array($endpoint, $this->endpoints)){
-			$url = rtrim($this->apiURL, " /") ."/". ltrim($endpoint, "/");
-			if($data){
-				$url = sprintf("%s?%s", rtrim($url, " ?"), $this->toQueryString($data));
-			}
-		// }else{
-		// 	throw new InvalidEndpointException("That endpoint doesn't exist");
-		// }
+		$url = rtrim($this->apiURL, " /") ."/". ltrim($endpoint, "/");
+		if($data){
+			$url = sprintf("%s?%s", rtrim($url, " ?"), $this->toQueryString($data));
+		}
 
 		return $url;
 	}
@@ -114,10 +127,75 @@ class Pinger {
 	 */
 	protected function ping($url, $context){
 		$stream = fopen($url, 'r', false, $context);
-		$this->responseInfo = stream_get_meta_data($stream);
-		$response = stream_get_contents($stream);
+
+		$this->responseMeta = stream_get_meta_data($stream);
+		$this->response     = stream_get_contents($stream);
+
 		fclose($stream);
-		return $response;
+
+		return $this->response;
 	}
+
+	/**
+	 * method to set additional headers to be sent with the request
+	 * @param array $headers An array of key => value pairs to use to create headers
+	 * @return
+	 */
+	function setHeaders(array $headers){
+		foreach($headers as $header => $value){
+			$this->headers[strtolower($header)] = trim($value);
+		}
+	}
+
+	/**
+	 * Method to combine the various information necessary to create a stream
+	 * context. POST requests send data here
+	 *
+	 * @param string $method The HTTP method
+	 * @param array $data The content of the request
+	 * @return resource
+	 */
+	protected function createHTTPContext($method, array $data = array()){
+
+		$opts = array("http" => array("method" => strtoupper($method)));
+
+		if($this->headers){
+			$opts["http"]["header"] = $this->assembleHeaders();
+		}
+
+		if($this->user_agent){ // can be set as a header
+			$opts["http"]["user_agent"] = $this->user_agent;
+		}
+
+		if($data){
+			$opts["http"]["content"] = $this->toQueryString($data);
+		}
+
+		return stream_context_create($opts);
+	}
+
+	/**
+	 * method to take an array of data and create a valid query string to send as
+	 * part of the URL or as the body of the request
+	 * @param array $data The data to encode
+	 * @return string
+	 */
+	protected function toQueryString(array $data = array()){
+		if(!$data){ return ""; }
+		return http_build_query($data, "no_", "&");
+	}
+
+	/**
+	 * method to normalize the headers
+	 * @return string
+	 */
+	protected function assembleHeaders(){
+		$finalHeader = array();
+		foreach($this->headers as $header => $value){
+			$finalHeader[] = sprintf("%s: %s", ucwords($header), trim($value));
+		}
+		return implode($this->DELIM, $finalHeader);
+	}
+
 }
 
